292ff823faee83d239391e8bfc73620f4d38e783
Eric Crudup <briefcasemanx@gmail.com>
2022-02-25 15:17:05 -0800

    added Program.cs for user interaction in console

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -51,12 +51,6 @@ namespace PierresBakery.Tests
       List<Pastry> pastes = new List<Pastry>{ new Pastry(2), new Pastry(2), new Pastry(2), new Pastry(2), new Pastry(2), new Pastry(2)};
       Assert.AreEqual(10, Pastry.PastryDiscounter(pastes));
     }
-    // [TestMethod]
-    // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
-    // {
-      // any necessary logic to prep for test; instantiating new classes, etc.
-      //Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
-    // }
   }
 
 }
---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using PierresBakery.Models;
+
+
+namespace PierresBakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+
+    }
+
+  }
+}
