8eda9c5f80f99fbd1bb6bab9c3107fd303440c5b
Eric Crudup <briefcasemanx@gmail.com>
2022-02-25 12:42:43 -0800

    Red: pastry constructor good fail

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -1,1 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfItemPastry_PastryInstance()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(int), newPastry.GetType());
+    }
+
+
+    // [TestMethod]
+    // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    // {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      //Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    // }
+  }
+
+}
