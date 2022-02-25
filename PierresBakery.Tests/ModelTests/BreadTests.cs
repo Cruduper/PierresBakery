01f0bb8b4c89bca49afe5df0d656b5fd3871a232
Eric Crudup <briefcasemanx@gmail.com>
2022-02-25 10:25:54 -0800

    Red: test for setting Bread Price good fail

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -11,8 +11,33 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfItemBread_BreadInstance()
     {
-      Bread newBread= new Bread();
+      Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
+    [TestMethod]
+    public void SetPrice_SetsPriceOfBreadTo5_5()
+    {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Bread newBread = new Bread();
+      newBread.Price = 5;
+      Assert.AreEqual(6, newBread.Price);
+    }
+
+    // public void BreadConstructor_SetsPriceOfBreadTo5UsingOverloadedBreadConstructor_5()
+    // {
+    //   // any necessary logic to prep for test; instantiating new classes, etc.
+    //   Bread newBread = new Bread(5);
+    //   Assert.AreEqual(6, newBread.Price);
+    // }
+
+
+
+    // [TestMethod]
+    // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    // {
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      //Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    // }
   }
 }
